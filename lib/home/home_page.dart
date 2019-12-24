91d9c17a6eda579217863e091c8f1f536a53958a
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-12-24 00:51:49 -0300

    mudando o stateless para statefull pela necessidade da tela ser dinamica

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,84 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> categories = [
+    "Android",
+    "Java",
+    "História",
+    "Ciencias",
+    "Flutter"
+  ];
+
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.only(top: 42),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 24,
+                    right: 24,
+                  ),
+                  child: Text(
+                    "Biblioteca",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text("Recomendados"),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 80,
+              child: ListView.builder(
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext c, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 6, right: 6),
+                    child: GestureDetector(
+                      onTap: () {
+                        _selectedIndex = index;
+                        setState(() {});
+                      },
+                      child: Chip(
+                        padding: EdgeInsets.only(left: 8, right: 8),
+                        backgroundColor: index == _selectedIndex
+                            ? Colors.blue
+                            : Colors.grey[200],
+                        label: Text(
+                          categories.elementAt(index),
+                          style: TextStyle(
+                            color: index == _selectedIndex
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
